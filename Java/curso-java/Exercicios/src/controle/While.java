051eef65b7ac5219b09e42045f5dd26637ccc713
Daniel Pedroso <hspedroso@gmail.com>
2022-10-17 10:15:04 -0300

    atualizado

---
+++ Java/curso-java/Exercicios/src/controle/While.java
@@ -0,0 +1,17 @@
+package controle;
+
+public class While {
+	
+	public static void main(String[] args) {
+		
+		
+		int contador = 0;						//definada variavel a ser incrementada
+				
+		while (contador <= 10) {				//inserida a condição do WHILE. Ira se repetir enquanto for True
+			System.out.println(contador);
+			contador++;							// adicionado +1 ao contador
+		} 		
+		}
+	}
+
+
