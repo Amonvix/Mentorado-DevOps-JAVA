3ca2eadbd6bfa084bbc48b2def234bccc877eb3e
Daniel Pedroso <hspedroso@gmail.com>
2022-10-17 10:22:32 -0300

    atualziado

---
+++ Java/curso-java/Exercicios/src/controle/DesafioIf.java
@@ -0,0 +1,20 @@
+package controle;
+
+public class DesafioIf {
+	
+	public static void main(String[] args) {
+		
+		
+		double nota = 1.3;
+		
+		if (nota >= 9) ;		// o erro esta nesta linha que contem um ponto e virgula, ele rouba a condicional do codigo, fazendo o texto ser exibido
+		{		
+			System.out.println("Quadro de Honra");
+			System.out.println("Parabéns!!!");
+		}
+				
+				
+		
+	}
+
+}
---
+++ Java/curso-java/Exercicios/src/controle/EstruturasDeControle/ExercicioFinal09.java
@@ -0,0 +1,28 @@
+package controle.EstruturasDeControle;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+public class ExercicioFinal09 {
+	
+//	9. Crie um programa que recebe 10 valores e ao final imprima o maior número.
+	
+	public static void main(String[] args) {
+		
+		Scanner enter = new Scanner(System.in);
+		ArrayList<int>numeros = new ArrayList<int>();
+		int i = 10;
+		
+		
+		System.out.print("Digite um valor :");
+		int num =  enter.nextInt();
+		
+		
+		
+		
+		
+		
+		enter.close();
+	}
+
+}
